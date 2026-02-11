fad5bd98bd5d6d4783c535ef22210885d5aa362c
jose <jose.ant1022@gmail.com>
2026-02-11 04:44:22 -0500

    Create _files directory

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,27 +1,31 @@
-import axios from "axios";
 import fs from "fs/promises";
 import path from "path";
-import { fileURLToPath } from "url";
+import axios from "axios";
 
-/**
- * Convierte una URL en un nombre de archivo válido
- * https://codica.la/cursos → codica-la-cursos.html
- */
-const getFileNameFromUrl = (url) => {
+const makeFileName = (url) => {
   const { hostname, pathname } = new URL(url);
+
   const name = `${hostname}${pathname}`
-    .replace(/[^a-zA-Z0-9]/g, "-")
-    .replace(/-+$/, "");
+    .replace(/\/$/, "")
+    .replace(/[^a-zA-Z0-9]/g, "-");
 
   return `${name}.html`;
 };
 
-export default (url, outputDir = process.cwd()) => {
-  const fileName = getFileNameFromUrl(url);
+const makeFilesDirName = (fileName) => fileName.replace(".html", "_files");
+
+const pageLoader = (url, outputDir = process.cwd()) => {
+  const fileName = makeFileName(url);
   const filePath = path.join(outputDir, fileName);
+
+  const filesDirName = makeFilesDirName(fileName);
+  const filesDirPath = path.join(outputDir, filesDirName);
 
   return axios
     .get(url)
     .then((response) => fs.writeFile(filePath, response.data))
+    .then(() => fs.mkdir(filesDirPath, { recursive: true }))
     .then(() => filePath);
 };
+
+export default pageLoader;
